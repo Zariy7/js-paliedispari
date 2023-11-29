932c4688c37b8ef0ea534bab1014d6b8b6a8ffd0
Zariy7 <nazariy.hereha@gmail.com>
2023-11-29 05:29:28 +0100

    js-palindroma comments added

--- js-palindroma/js/script.js
+++ js-palindroma/js/script.js
@@ -2,15 +2,24 @@ let btn = document.getElementById('checkBtn');
 btn.addEventListener('click', palCheck);
 
 function palCheck(){
+    //GET INPUT WORD FROM USER
     let word = document.getElementById('wordCheck').value;
+
+    //MAKE INPUT WORD ALL LOWERCASE
     word = word.toLowerCase();
+
+    //TURN INPUT WORD INTO AN ARRAY
     let wordArray = word.split('');
 
+    //REMOVE ALL PUNCTUATION
     removePunctuation(wordArray);
-    console.log(wordArray);
-    let reverseWord = wordArray.toReversed();
-    console.log(reverseWord);
+    //console.log(wordArray);
 
+    //MAKE A SECOND ARRAY THAT IS THE REVERSE OF THE FIRST ARRAY
+    let reverseWord = wordArray.toReversed();
+    //console.log(reverseWord);
+
+    //TURN BOTH ARRAYS INTO STRINGS AGAIN AND CHECK IF THEY ARE EXACTLY THE SAME
     if(wordArray.join('') === reverseWord.join('')){
         document.getElementById('result').innerText = `E' un palindromo!`;
     }
@@ -20,15 +29,19 @@ function palCheck(){
 }
 
 function removePunctuation(array){
-    let removeCharacters = [' ', ',', '-', '.', '_', '/', "'", '"', '?', '!']
+    let removeCharacters = [' ', ',', '-', '.', '_', '/', "'", '"', '?', '!'];
 
+    //CHECKS EVERY ELEMENT INSIDE THE INPUT ARRAY
     for(let i=0; i<array.length; i++){
+        //COMPARES THEM ALL TO THE REMOVECHARACTERS ARRAY ELEMENTS
         for(let j=0; j<removeCharacters.length; j++){
+            //IF IT FINDS A MATCH IT REMOVES THAT ELEMENT FROM THE INPUT ARRAY
             if(array[i] === removeCharacters[j]){
                 array.splice(i, 1);
             }
         }
     }
-
+    
+    //AND THEN IT RETURNS THE INPUT ARRAY BACK FROM WHENCE IT CAME
     return array;
 }
